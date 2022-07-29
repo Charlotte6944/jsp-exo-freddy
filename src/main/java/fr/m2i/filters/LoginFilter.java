d6ba256092f63455ecb3229c6c74e02bc5f39c88
Charlotte Marion <marion.charlotte@orange.fr>
2022-07-29 10:43:58 +0200

    modif

---
+++ src/main/java/fr/m2i/filters/LoginFilter.java
@@ -0,0 +1,56 @@
+package fr.m2i.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+@WebFilter({"/secretone", "/secrettwo"})
+public class LoginFilter implements Filter {
+	
+	private static String PAGE = "/WEF-INF/pages/login.jsp";
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		Object req = (Object) request.getAttribute("auth");
+		
+		if (req == null) {
+			HttpServletResponse httpresponse = (HttpServletResponse) response;
+			httpresponse.sendRedirect(PAGE);
+		} else {
+			chain.doFilter(request, response);			
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
